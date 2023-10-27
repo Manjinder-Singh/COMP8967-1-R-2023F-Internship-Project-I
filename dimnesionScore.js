ca8069b6ce1940527e56579ccd7f0aed915e0e77
Karan <KaranVishavjit@gmail.com>
2023-10-27 12:10:04 +0530

    Signup integration and dimnesionScore update

--- dimnesionScore.js
+++ dimnesionScore.js
@@ -1,13 +1,37 @@
 // Given values
 const minimum = 10;
-const maximum = 100; 
-const x = 50;
+const maximum = 100;
 const weight = 0.5;
 
-// Calculate linear score
-const linearScore = (x - minimum) / (maximum - minimum);
+// Define an array of 'x' values for each scenario
+const scenario1XValues = [50, 60, 70];
+const scenario2XValues = [30, 40, 50];
 
-// Calculate dimensionless score  
-const dimensionlessScore = weight * linearScore;
+// Function to calculate the total dimensionless score for a scenario
+function calculateTotalDimensionlessScore(xValues) {
+  let totalDimensionlessScore = 0;
 
-console.log(dimensionlessScore);
+  for (const x of xValues) {
+    // Calculate linear score
+    const linearScore = (x - minimum) / (maximum - minimum);
+
+    // Calculate dimensionless score
+    const dimensionlessScore = weight * linearScore;
+
+    // Add the dimensionless score to the total
+    totalDimensionlessScore += dimensionlessScore;
+  }
+
+  return totalDimensionlessScore;
+}
+
+// Calculate and print the total dimensionless score for each scenario
+const totalScoreScenario1 = calculateTotalDimensionlessScore(scenario1XValues);
+const totalScoreScenario2 = calculateTotalDimensionlessScore(scenario2XValues);
+
+console.log("Total Dimensionless Score for Scenario 1:", totalScoreScenario1);
+console.log("Total Dimensionless Score for Scenario 2:", totalScoreScenario2);
+
+// Calculate and print the maximum value of total scores
+const maxTotalScore = Math.max(totalScoreScenario1, totalScoreScenario2);
+console.log("Maximum Total Dimensionless Score:", maxTotalScore);
