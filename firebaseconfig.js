32c5847139e0f413a19e6b5017d79f87ac7d47b4
Karan <KaranVishavjit@gmail.com>
2023-11-20 02:09:30 +0530

    Session monitoring enabled, if logged in users are redirected to tableops_updated_download page, if not users are redirected to login page.

---
+++ firebaseconfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDDtV2A8BZD2RQ6NlWkcPJn7nUnupKtXg8",
+    authDomain: "climateneutral-e5e14.firebaseapp.com",
+projectId: "climateneutral-e5e14",
+storageBucket: "climateneutral-e5e14.appspot.com",
+messagingSenderId: "823275659630",
+appId: "1:823275659630:web:26433c0cac4d8423ea217f"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
